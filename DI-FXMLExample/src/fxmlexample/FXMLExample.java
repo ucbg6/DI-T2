0416f7172a2d65c12403ec58a1acf7c3ddb893ed
Uriel C <71645614+ucbg6@users.noreply.github.com>
2021-05-10 06:58:01 +0200

    Finalizado

---
+++ DI-FXMLExample/src/fxmlexample/FXMLExample.java
@@ -0,0 +1,34 @@
+
+package fxmlexample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Aldobg
+ */
+public class FXMLExample extends Application {
+    
+@Override
+    public void start(Stage stage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getResource("FXMLExample.fxml"));
+    
+        Scene scene = new Scene(root, 300, 275);
+    
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
